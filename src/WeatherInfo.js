9cb798019b2a371375289f4769b9a980e327b3cb
flimby2010 <87784870+flimby2010@users.noreply.github.com>
2021-10-08 10:00:12 -0300

    Installed Bootstrap

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -11,26 +11,26 @@ export default function WeatherInfo(props) {
         <li>
           <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
-      <div className="row mt-3">
+         </ul>
+      <div className="row">
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
-
+ <div className="col-6">
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
-            </div>
+            </div> </div>
           </div>
         </div>
-        <div className="col-6">
+        
           <ul>
+          <li className="text-capitalize">{props.data.description}</li>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
-        </div>
+        
       </div>
     </div>
   );
